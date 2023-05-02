86360c435225fca16e2af12095e124ec806f2684
Yohanes Rizky Gumilir <yohanes.rizky@ai.astra.co.id>
2023-05-02 15:38:41 +0700

    Initial commit

---
+++ app/src/main/java/com/yoriworks/learnnavigationcompose/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.yoriworks.learnnavigationcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.yoriworks.learnnavigationcompose.screen.DetailScreen
+import com.yoriworks.learnnavigationcompose.screen.HomeScreen
+import com.yoriworks.learnnavigationcompose.ui.theme.LearnNavigationComposeTheme
+
+class MainActivity : ComponentActivity() {
+    
+    sealed class NavDrawerItem(var route: String, var icon: Int, var title: String) {
+        object Home: NavDrawerItem("home", R.drawable.ic_menu_home, "Home")
+        object Detail : NavDrawerItem("detail", android.R.drawable.ic_menu_info_details, "Detail")
+    }
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearnNavigationComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    MyAppNavHost(navController = navController)
+                }
+            }
+        }
+    }
+    
+}
+
+@Composable
+fun MyAppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = "home"
+) {
+    NavHost(
+        modifier = modifier, navController = navController, startDestination = startDestination
+    ) {
+        composable("home") { HomeScreen(navController) }
+        composable("detail") { DetailScreen(navController) }
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/yoriworks/learnnavigationcompose/screen/HomeScreen.kt
@@ -0,0 +1,36 @@
+package com.yoriworks.learnnavigationcompose.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.navigation.NavHostController
+import com.yoriworks.learnnavigationcompose.MainActivity
+import com.yoriworks.learnnavigationcompose.R
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .wrapContentSize(Alignment.Center),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home Screen", style = MaterialTheme.typography.titleLarge)
+        Button(onClick = { navController.navigate(route = MainActivity.NavDrawerItem.Detail.route) }) {
+            Text(text = "Detail")
+        }
+        
+    }
+}
